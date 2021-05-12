7b985a23c355d666e9d506892c4cae4df445b071
SBC <lochan.sharad@gmail.com>
2021-05-12 20:04:27 +0800

    Npm inititated and dependencies inquirer and prettier installed. epmloyee test file created and passed employee test.

---
+++ __tests__/employee.test.js
@@ -0,0 +1,28 @@
+const employee = require('../lib/employee');
+
+describe("Employee", () => {
+
+    it('Creates new employee with name,id,email when receives name,id,email  ', () => {
+
+        expect(new employee("sar", "003", "hey@email.com")).toEqual({ name: "sar", id: "003", email: "hey@email.com" });
+    });
+
+    it("Returns employee name when call getname()", () => {
+        const NewEmp = new employee("sar", "003", "hey@email.com");
+        expect(NewEmp.getName()).toEqual('sar');
+
+    });
+
+    it("Returns employee id when call getId()", () => {
+        const NewEmp = new employee("sar", "003", "hey@email.com");
+        expect(NewEmp.getId()).toEqual('003');
+
+    });
+
+    it("Returns employee email when call getEmail()", () => {
+        const NewEmp = new employee("sar", "003", "hey@email.com");
+        expect(NewEmp.getEmail()).toEqual('hey@email.com');
+
+    });
+
+})
